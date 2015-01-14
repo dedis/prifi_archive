764d7f94ef9a7c959ca3be52a142dde9686218e3
Dylan Visher <dylan.visher@yale.edu>
2015-01-13 21:38:00 -0500

    adding config support

---
+++ coco/config.go
@@ -0,0 +1,77 @@
+package coco
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io/ioutil"
+)
+
+/*
+file format: json
+
+ex.json
+{
+	hosts: ["host1", "host2", "host3"],
+	tree: {host1:{
+		    host2:{},
+			host3:{}}
+		  }
+}
+
+*/
+
+type HostConfig struct {
+	Hosts map[string]*HostNode // maps hostname to host
+	Dir   *directory
+}
+
+func NewHostConfig() *HostConfig {
+	return &HostConfig{make(map[string]*HostNode), newDirectory()}
+}
+
+// Depth-First search construction of the host tree
+func ConstructTree(tree map[string]interface{}, hc *HostConfig) error {
+	// get the first key in the tree there should only be one
+	for k, v := range tree {
+		if _, ok := hc.Hosts[k]; !ok {
+			fmt.Println("unknown host in tree:", k)
+			return errors.New("unknown host in tree")
+		}
+
+		h := hc.Hosts[k]
+		// this key should map to a map[string]interface{} that represents the
+		// rest of the tree
+		rest := v.(map[string]interface{})
+		for child := range rest {
+			gp, err := NewGoPeer(hc.Dir, child)
+			if err != nil {
+				h.AddChildren(gp)
+			}
+		}
+		ConstructTree(rest, hc)
+	}
+	return nil
+}
+
+func ReadConfig(fname string) (*HostConfig, error) {
+	hc := NewHostConfig()
+	file, err := ioutil.ReadFile(fname)
+	if err != nil {
+		return hc, err
+	}
+	var m map[string]interface{}
+	json.Unmarshal(file, &m)
+	// read the hosts lists
+	hnames := m["hosts"].([]string)
+	for _, h := range hnames {
+		// add to the hosts list if we havent added it before
+		if _, ok := hc.Hosts[h]; !ok {
+			hc.Hosts[h] = NewHostNode(h)
+		}
+	}
+	tree := m["tree"].(map[string]interface{})
+	ConstructTree(tree, hc)
+	// construct the host tree
+	return hc, nil
+}
--- coco/tree.go
+++ coco/tree.go
@@ -13,10 +13,10 @@ type Peer interface {
 	Get() interface{}
 }
 
-// A TreeNode has the ability to send messages up the tree (parents),
+// A Host has the ability to send messages up the tree (parents),
 // get from the parents, send messages to the children, and get messages from
 // the children.
-type TreeNode interface {
+type Host interface {
 	PutUp(interface{})
 	GetUp() interface{}
 	PutDown(interface{})
@@ -25,53 +25,55 @@ type TreeNode interface {
 
 // host embodies the local state of a single host in the network
 // it satisfies the tree node interface.
-type Host struct {
+type HostNode struct {
 	hostname string
 	parent   Peer
 	children map[string]Peer // map so we only add unique peers
 }
 
-func NewHost(hostname string) *Host {
-	h := &Host{hostname: hostname,
+func NewHostNode(hostname string) *HostNode {
+	h := &HostNode{hostname: hostname,
 		children: make(map[string]Peer)}
 	return h
 }
 
-func (h Host) AddParent(p Peer) {
+func (h HostNode) AddParent(p Peer) {
 	h.parent = p
 }
 
-// TODO: only add new children
-func (h Host) AddChildren(ps ...Peer) {
+func (h HostNode) AddChildren(ps ...Peer) {
 	for _, p := range ps {
 		h.children[p.Name()] = p
 	}
 }
 
-func (h Host) PutUp(data interface{}) {
+// TODO(dyv): following methods will have to be rethought with network failures and
+// latency in mind. What happens during GetDown if one node serves up old
+// responses?
+func (h HostNode) PutUp(data interface{}) {
 	h.parent.Put(data)
 }
-func (h Host) GetUp() interface{} {
+func (h HostNode) GetUp() interface{} {
 	return h.parent.Get()
 }
 
-func (h Host) PutDown(data interface{}) {
+func (h HostNode) PutDown(data interface{}) {
 	for _, c := range h.children {
 		c.Put(data)
 	}
 }
-func (h Host) GetDown() []interface{} {
+func (h HostNode) GetDown() []interface{} {
 	var mu sync.Mutex
 	data := make([]interface{}, len(h.children))
 	var wg sync.WaitGroup
 	for _, c := range h.children {
 		wg.Add(1)
-		go func() {
+		go func(c Peer) {
 			d := c.Get()
 			mu.Lock()
 			data = append(data, d)
 			mu.Unlock()
-		}()
+		}(c)
 	}
 	wg.Wait()
 	return data
@@ -82,11 +84,13 @@ func (h Host) GetDown() []interface{} {
 // connections).
 type directory struct {
 	sync.Mutex
-	channel map[string]chan interface{}
+	channel    map[string]chan interface{}
+	nameToPeer map[string]*goPeer
 }
 
 func newDirectory() *directory {
-	return &directory{channel: make(map[string]chan interface{})}
+	return &directory{channel: make(map[string]chan interface{}),
+		nameToPeer: make(map[string]*goPeer)}
 }
 
 type goPeer struct {
@@ -104,10 +108,14 @@ func NewGoPeer(dir *directory, hostname string) (*goPeer, error) {
 	if _, ok := gp.dir.channel[hostname]; ok {
 		// return the already existant peer
 		fmt.Println("Peer Already Exists")
-		return nil, PeerExists
+		return gp.dir.nameToPeer[hostname], PeerExists
 	}
 	gp.dir.channel[hostname] = make(chan interface{})
 	return gp, nil
+}
+
+func (p goPeer) Name() string {
+	return p.hostname
 }
 
 func (p goPeer) Put(data interface{}) {
