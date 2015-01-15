d66dc89530109c5a6902892154d129c46e15cc45
Dylan Visher <dylan.visher@yale.edu>
2015-01-14 19:40:54 -0500

    Added / fixed config.go. Still need to test.

--- coco/config.go
+++ coco/config.go
@@ -8,6 +8,7 @@ import (
 )
 
 /*
+Example configuration file.
 file format: json
 
 ex.json
@@ -19,43 +20,59 @@ ex.json
 		  }
 }
 
+This will be compiled into the specified tree structure with HostNode Hosts and goPeer Peers.
 */
 
+// HostConfig stores all of the relevant information of the configuration file.
 type HostConfig struct {
 	Hosts map[string]*HostNode // maps hostname to host
-	Dir   *directory
+	Dir   *directory           // the directory mapping hostnames to goPeers
+	Root  *HostNode            // the host root of the tree
 }
 
+// NewHostConfig creates a new host configuration that can be populated with
+// hosts.
 func NewHostConfig() *HostConfig {
-	return &HostConfig{make(map[string]*HostNode), newDirectory()}
+	return &HostConfig{Hosts: make(map[string]*HostNode), Dir: newDirectory(), Root: nil}
 }
 
-// Depth-First search construction of the host tree
+// ConstructTree does a depth-first construction of the tree specified in the
+// config file. It detects unknown hosts but does not detect the error of
+// multiple root nodes, rather it silently choses just one. ConstructTree must
+// be call AFTER populating the HostConfig with ALL the possible hosts.
 func ConstructTree(tree map[string]interface{}, hc *HostConfig, parent Peer) error {
-	// get the first key in the tree there should only be one
-	for k, v := range tree {
+	// each k will be a sibling in the tree
+	// for each sibling add its children
+	for k, subtree := range tree {
 		if _, ok := hc.Hosts[k]; !ok {
 			fmt.Println("unknown host in tree:", k)
 			return errors.New("unknown host in tree")
 		}
-
 		h := hc.Hosts[k]
-		h.parent = parent
-		// this key should map to a map[string]interface{} that represents the
-		// rest of the tree
-		rest := v.(map[string]interface{})
-		for child := range rest {
-			gp, err := NewGoPeer(hc.Dir, child)
-			if err != nil {
-				h.AddChildren(gp)
-			}
+		if parent == nil {
+			fmt.Printf("root node is %v\n", h.name)
+			hc.Root = h
 		}
-		ConstructTree(rest, hc, hc.Dir.nameToPeer[h.name])
+		h.parent = parent
+
+		children := subtree.(map[string]interface{})
+		for child := range children {
+			// ignore the error because we don't care if we have already
+			// constructed this peer before.
+			gp, _ := NewGoPeer(hc.Dir, child)
+			h.AddChildren(gp)
+			fmt.Printf("added %v as child of %v\n", gp.hostname, h.name)
+		}
+		if err := ConstructTree(children, hc, hc.Dir.nameToPeer[h.name]); err != nil {
+			return err
+		}
 	}
 	return nil
 }
 
-func ReadConfig(fname string) (*HostConfig, error) {
+// LoadConfig loads a configuration file in the format specified above. It
+// populates a HostConfig with HostNode Hosts and goPeer Peers.
+func LoadConfig(fname string) (*HostConfig, error) {
 	hc := NewHostConfig()
 	file, err := ioutil.ReadFile(fname)
 	if err != nil {
@@ -72,7 +89,7 @@ func ReadConfig(fname string) (*HostConfig, error) {
 		}
 	}
 	tree := m["tree"].(map[string]interface{})
-	ConstructTree(tree, hc, nil)
+	err = ConstructTree(tree, hc, nil)
 	// construct the host tree
-	return hc, nil
+	return hc, err
 }
