5b27876ead6e4c0fdc05858632dc77201fd1e878
Iulia Tamas <iulia.tamas@yale.edu>
2015-01-26 23:37:23 -0500

    refactoring to acommodate integration with Collective Signing nodes

--- timestamp/server.go
+++ timestamp/server.go
@@ -22,14 +22,25 @@ type Server struct {
 	name    string
 	clients map[string]*coco.GoConn
 	nRounds int
+	mux     sync.Mutex
 
 	dir *coco.GoDirectory
+
+	coco.SigningNode
+
+	// for aggregating messages from clients
+	Queue      [][]MustReplyMessage
+	READING    int
+	PROCESSING int
 }
 
 func NewServer(name string, dir *coco.GoDirectory, suite abstract.Suite) (s *Server) {
 	s = &Server{name: name, dir: dir}
 	s.clients = make(map[string]*coco.GoConn)
 	s.suite = suite
+
+	s.READING = 0
+	s.PROCESSING = 1
 	return
 }
 
@@ -41,11 +52,12 @@ type MustReplyMessage struct {
 }
 
 // TODO: error handling
-func (s *Server) Listen() {
-	var mux sync.Mutex
-	READING := 0
-	PROCESSING := 1
-	Queue := make([][]MustReplyMessage, 2)
+func (s *Server) Listen(isRoot bool) {
+	s.Queue = make([][]MustReplyMessage, 2)
+	Queue := s.Queue
+	READING := s.READING
+	PROCESSING := s.PROCESSING
+
 	Queue[READING] = make([]MustReplyMessage, 0)
 	Queue[PROCESSING] = make([]MustReplyMessage, 0)
 	for _, c := range s.clients {
@@ -59,52 +71,69 @@ func (s *Server) Listen() {
 					fmt.Println("Message of unknown type")
 				case StampRequestType:
 					// fmt.Println("Server getting message", tsm)
-					mux.Lock()
+					s.mux.Lock()
 					Queue[READING] = append(Queue[READING],
 						MustReplyMessage{tsm: tsm, to: c.Name()})
-					mux.Unlock()
+					s.mux.Unlock()
 				}
 			}
 		}(c)
 	}
 
-	ticker := time.Tick(1 * time.Second)
-	for _ = range ticker {
-		mux.Lock()
-		READING, PROCESSING = PROCESSING, READING
-		Queue[READING] = Queue[READING][:0]
-		mux.Unlock()
-
-		// give up if nothing to process
-		if len(Queue[PROCESSING]) == 0 {
-			continue
-		}
-
-		// count only productive rounds
-		s.nRounds++
-
-		// pull out to be Merkle Tree leaves
-		leaves := make([]HashId, 0)
-		for _, msg := range Queue[PROCESSING] {
-			leaves = append(leaves, HashId(msg.tsm.sreq.Val))
-		}
-
-		// create Merkle tree for this round
-		mtRoot, proofs := ProofTree(s.suite.Hash, leaves)
-		if CheckProofs(s.suite.Hash, mtRoot, leaves, proofs) == true {
-			fmt.Println("Proofs successful for round " + strconv.Itoa(s.nRounds))
-		} else {
-			panic("Proofs unsuccessful for round " + strconv.Itoa(s.nRounds))
-		}
-
-		for i, msg := range Queue[PROCESSING] {
-			s.Put(msg.to,
-				TimeStampMessage{
-					Type:  StampReplyType,
-					ReqNo: msg.tsm.ReqNo,
-					srep:  &StampReply{Sig: mtRoot, Prf: proofs[i]}})
+	if isRoot == true {
+		fmt.Println(s.Name(), "is root")
+		ticker := time.Tick(1 * time.Second)
+		for _ = range ticker {
+			// send an announcement message to all other servers
+			// s.Announce(&coco.AnnouncementMessage{LogTest: []byte("New Round")})
+			s.AggregateCommits()
 		}
 	}
+}
+
+func (s *Server) AggregateCommits() {
+	s.mux.Lock()
+	// get data from s once
+	Queue := s.Queue
+	READING := s.READING
+	PROCESSING := s.PROCESSING
+
+	READING, PROCESSING = PROCESSING, READING
+	Queue[READING] = Queue[READING][:0]
+
+	// give up if nothing to process
+	if len(Queue[PROCESSING]) == 0 {
+		return
+	}
+
+	// count only productive rounds
+	s.nRounds++
+
+	// pull out to be Merkle Tree leaves
+	leaves := make([]HashId, 0)
+	for _, msg := range Queue[PROCESSING] {
+		leaves = append(leaves, HashId(msg.tsm.sreq.Val))
+	}
+	s.mux.Unlock()
+
+	// create Merkle tree for this round
+	mtRoot, proofs := ProofTree(s.suite.Hash, leaves)
+	if CheckProofs(s.suite.Hash, mtRoot, leaves, proofs) == true {
+		fmt.Println("Proofs successful for round " + strconv.Itoa(s.nRounds))
+	} else {
+		panic("Proofs unsuccessful for round " + strconv.Itoa(s.nRounds))
+	}
+
+	s.mux.Lock()
+	// sending replies back to clients
+	for i, msg := range Queue[PROCESSING] {
+		s.Put(msg.to,
+			TimeStampMessage{
+				Type:  StampReplyType,
+				ReqNo: msg.tsm.ReqNo,
+				srep:  &StampReply{Sig: mtRoot, Prf: proofs[i]}})
+	}
+	s.mux.Unlock()
 }
 
 func (s *Server) Name() string {
--- timestamp/server_test.go
+++ timestamp/server_test.go
@@ -9,10 +9,37 @@ import (
 	"github.com/dedis/prifi/coco"
 )
 
+// Create nClients for the server, with first client associated with number fClient
+func createClientsForServer(nClients int, server *Server, dir *coco.GoDirectory, fClient int) []*Client {
+	clients := make([]*Client, 0, nClients)
+	for i := 0; i < nClients; i++ {
+		clients = append(clients, NewClient("client"+strconv.Itoa(fClient+i), dir))
+
+		// intialize server conn to client
+		ngc, err := coco.NewGoConn(dir, server.Name(), clients[i].Name())
+		if err != nil {
+			panic(err)
+		}
+		server.clients[clients[i].Name()] = ngc
+
+		// intialize client connection to server
+		ngc, err = coco.NewGoConn(dir, clients[i].Name(), server.Name())
+		if err != nil {
+			panic(err)
+		}
+		clients[i].servers[server.Name()] = ngc
+
+		go clients[i].Listen()
+		go clients[i].showHistory()
+	}
+
+	return clients
+}
+
 func TestStaticMultipleClients(t *testing.T) {
-	nClients := 10
+	nClients := 2
 	nMessages := 4 // per round
-	nRounds := 5
+	nRounds := 2
 
 	// Crypto setup
 	suite := nist.NewAES128SHA256P256()
@@ -21,33 +48,14 @@ func TestStaticMultipleClients(t *testing.T) {
 
 	// create server, clients
 	server := NewServer("server", dir, suite)
-	client := make([]*Client, 0, nClients)
-	for i := 0; i < nClients; i++ {
-		client = append(client, NewClient("client"+strconv.Itoa(i), dir))
+	clients := createClientsForServer(nClients, server, dir, 0)
 
-		// intialize server conn to client
-		ngc, err := coco.NewGoConn(dir, server.Name(), client[i].Name())
-		if err != nil {
-			panic(err)
-		}
-		server.clients[client[i].Name()] = ngc
-
-		// intialize client connection to server
-		ngc, err = coco.NewGoConn(dir, client[i].Name(), server.Name())
-		if err != nil {
-			panic(err)
-		}
-		client[i].servers[server.Name()] = ngc
-
-		go client[i].Listen()
-		go client[i].showHistory()
-	}
-
-	go server.Listen()
+	isRoot := true
+	go server.Listen(isRoot)
 
 	// have client send messages
 	for r := 0; r < nRounds; r++ {
-		for _, client := range client {
+		for _, client := range clients {
 
 			for i := 0; i < nMessages; i++ {
 				// TODO: messages should be sent hashed eventually
@@ -59,4 +67,36 @@ func TestStaticMultipleClients(t *testing.T) {
 		// wait between rounds
 		time.Sleep(3 * time.Second)
 	}
+}
+
+// Configuration file data/exconf.json
+//       0
+//      / \
+//     1   4
+//    / \   \
+//   2   3   5
+func TestTreeFromStaticConfig(t *testing.T) {
+	return
+	hostConfig, err := coco.LoadConfig("data/exconf.json")
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = hostConfig.Run()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// create servers
+	nServers := len(hostConfig.SNodes)
+	servers := make([]*Server, 0, nServers)
+	for i, sn := range hostConfig.SNodes {
+		server := &Server{name: "server" + strconv.Itoa(i)}
+		server.SigningNode = *sn
+		servers = append(servers, server)
+
+	}
+	// Have root node initiate the signing protocol
+	// via a simple annoucement
+	hostConfig.SNodes[0].LogTest = []byte("Hello World")
+	hostConfig.SNodes[0].Announce(&coco.AnnouncementMessage{hostConfig.SNodes[0].LogTest})
 }
