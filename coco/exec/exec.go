eda1e2046aea69287d4a6b52f18fd66fc63014de
Dylan Visher <dylan.visher@yale.edu>
2015-01-26 22:35:41 -0500

    Deployment works on Zoo
    Deployment currently works on the Zoo. You have to get a little bit
    lucky in terms of port usage. Sometimes other people will be using
    the ports that you need. You can see this when you see a "tcp bind"
    error show up in the log messages. This code has to be cleaned up a
    little bit to become more reliable.
    
    This deployment process should work the same anywhere that remote ssh
    key pairs have been set up with, as long as hosts are either provided
    in the command line or in the configuration file itself.
    
    Next on the horizon is that we need to be able to specify a specific
    username to use when logging into machines. In addition, the configuration
    file and the deployment tool should add a option for just renaming the
    ports of the configuration file. This way hostnames can remane fixed
    but if port usage is not working out (or old ports have not been freed)
    then we can change the ports that we are using rather than waiting for
    the OS to garbage collect them.
    
    We also need to add a way for the deployment script to learn when the
    collective signing nodes have completed and whether they were successful.

--- coco/exec/exec.go
+++ coco/exec/exec.go
@@ -10,9 +10,13 @@
 package main
 
 import (
+	"bufio"
 	"flag"
+	"fmt"
+	"io"
 	"log"
 	"net"
+	"os"
 	"time"
 
 	"github.com/dedis/prifi/coco"
@@ -29,22 +33,34 @@ func init() {
 }
 
 func main() {
+	flag.Parse()
+	fmt.Println("Execing")
 	// open connection with remote logging interface if there is one
 	if logger != "" {
 		conn, err := net.Dial("tcp", logger)
 		if err != nil {
-			log.Println("ERROR: error establishing logging connection: ", err)
+			fmt.Println("ERROR ESTABLISHING LOG CONNECTION")
+			os.Exit(1)
+			log.Fatal("ERROR: error establishing logging connection: ", err)
 		} else {
-			log.SetOutput(conn)
+			log.Println("Log Test")
+			fmt.Println("Connected to logger successfully")
+			log.SetOutput(io.MultiWriter(os.Stdout, bufio.NewWriter(conn)))
+			log.SetPrefix(hostname + ":")
+			log.Println("Log Test")
+			fmt.Println("exiting logger block")
 		}
 	}
-	flag.Parse()
 	if hostname == "" {
+		fmt.Println("hostname is empty")
 		log.Fatal("no hostname given")
 	}
 	// open the testfile
+	time.Sleep(5 * time.Second)
+	fmt.Println("loading configuration")
 	hc, err := coco.LoadConfig(configFile, coco.ConfigOptions{ConnType: "tcp", Host: hostname})
 	if err != nil {
+		fmt.Println(err)
 		log.Fatal(err)
 	}
 	err = hc.Run(hostname)
