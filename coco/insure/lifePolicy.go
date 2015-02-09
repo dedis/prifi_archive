37c4e9e7090fbd1d4a9cff931b943edea4e50fa0
webiii <webiii@localhost>
2015-02-09 07:25:49 -0500

    Adding sending/receiving code for requesting a life policy

--- coco/insure/lifePolicy.go
+++ coco/insure/lifePolicy.go
@@ -1,10 +1,14 @@
 package insure
 
 import (
+	"container/list"
+	
 	"github.com/dedis/crypto/abstract"
 	"github.com/dedis/crypto/config"
 	"github.com/dedis/crypto/poly"
 	"github.com/dedis/crypto/random"
+	
+	"github.com/dedis/prifi/coco/connMan"
 )
 
 /* This file provides an implementation of the Policy interface via
@@ -28,8 +32,8 @@ type LifePolicy struct {
 	insurersList []abstract.Point
 
 	// Digitial Signatures that serve as "proof of insurance"
-	// TODO: Determine what type of proof I want to use.
-	proofList int
+	// The PolicyApprovedMessage contains the signatures.
+	proofList *list.List
 }
 
 /* This function selects a set of servers to serve as insurers.
@@ -65,7 +69,7 @@ func (lp *LifePolicy) GetInsurers() []abstract.Point {
 }
 
 // Returns the certificates of the insurers for each policy.
-func (lp *LifePolicy) GetPolicyProof() int {
+func (lp *LifePolicy) GetPolicyProof() *list.List {
 	return lp.proofList
 }
 
@@ -79,6 +83,7 @@ func (lp *LifePolicy) GetPolicyProof() int {
  *   keyPair         = the public/private key of the server
  *   serverList      = a list of the public keys of possible insurers
  *   selectInsurers  = a function for selecting insurers
+ *   cman            = the connection manager for sending messages.
  *   t               = the minimum number of shares to reconstruct the secret
  *   n               = the total shares to be distributed
  *
@@ -89,7 +94,7 @@ func (lp *LifePolicy) GetPolicyProof() int {
 
 func (lp *LifePolicy) TakeOutPolicy(keyPair config.KeyPair, serverList []abstract.Point,
 	selectInsurers func([]abstract.Point, int) ([]abstract.Point, bool),
-	t int, n int) (*LifePolicy, bool) {
+	cman connMan.ConnManager, t int, n int) (*LifePolicy, bool) {
 
 	// Initialize the policy.
 	ok := true
@@ -109,7 +114,7 @@ func (lp *LifePolicy) TakeOutPolicy(keyPair config.KeyPair, serverList []abstrac
 		}
 	}
 	//TODO: Use bytes maybe?
-	//lp.proofList = make([][]byte, len(lp.insurersList))
+	lp.proofList = new(list.List)
 
 	// Create a new polynomial from the private key where t
 	// shares are needed to reconstruct the secret. Then, split it
@@ -119,9 +124,42 @@ func (lp *LifePolicy) TakeOutPolicy(keyPair config.KeyPair, serverList []abstrac
 	prishares := new(poly.PriShares).Split(pripoly, n)
 	pubPoly := new(poly.PubPoly).Commit(pripoly, keyPair.Public)
 
-	// TODO: Send the shares off to the insurers
 
-	// TODO: Receive digital signatures from the others.
+	// Send each share off to the appropriate server.
+	for i := 0; i < n; i++ {
+		requestMsg := new(RequestInsuranceMessage).createMessage(prishares.Share(i), pubPoly)
+		cman.Put(lp.insurersList[i], new(PolicyMessage).createRIMessage(requestMsg))	
+	}
+
+	insurersListTemp := make([]abstract.Point, len(lp.insurersList))
+	copy(insurersListTemp, lp.insurersList)
+
+	// TODO: Add a timeout such that this process will end after a certain
+	// time and a new batch of insurers can be picked.
+	// TODO: Make it so that it stops as soon as we get R other insurers
+	// for t <= r <= n
+	for lp.proofList.Len() < n {
+
+		for i := 0; i < n; i++ {
+			// If we have already received a certificate for this
+			// node, move on.
+			if insurersListTemp[i] == nil {
+				continue
+			}
+		
+			msg := new(PolicyMessage)
+			cman.Get(lp.insurersList[i], msg)
+			
+			// If we got an approve message and it is valid, add it
+			// to our list of proofs and remove the node from the
+			// temporary insurer list.
+			if msg.Type == PolicyApproved &&
+			   msg.getPAM().verifyCertificate(keyPair.Suite, keyPair.Public){
+				insurersListTemp[i] = nil
+				lp.proofList.PushBack(msg.getPAM())
+			}
+		}
+	}
 
 	return lp, ok
 }
