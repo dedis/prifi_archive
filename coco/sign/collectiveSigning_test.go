dfb1d21acd1d980ab12c9490579f2e62d82eb973
Iulia Tamas <iulia.tamas@yale.edu>
2015-03-12 17:11:14 -0400

    added clarification of what viewchange tests in sign achieve

--- coco/sign/collectiveSigning_test.go
+++ coco/sign/collectiveSigning_test.go
@@ -291,6 +291,9 @@ func TestTCPStaticConfigRounds(t *testing.T) {
 	}
 }
 
+// Tests the integration of View Change with Signer (ability to reach consensus on a view change)
+// After achieving consensus, View is not actually changed, because of Signer test framework limitations
+// See tests in stamp/ for the actual view change ocurring
 // Go channels, static configuration, multiple rounds
 func TestViewChangeChan(t *testing.T) {
 	if testing.Short() {
@@ -326,7 +329,9 @@ func TestViewChangeChan(t *testing.T) {
 	}
 }
 
-// TCP connections, static configuration, multiple rounds
+// Tests the integration of View Change with Signer (ability to reach consensus on a view change)
+// After achieving consensus, View is not actually changed, because of Signer test framework limitations
+// See tests in stamp/ for the actual view change ocurring
 func TestViewChangeTCP(t *testing.T) {
 	time.Sleep(5 * time.Second)
 	if testing.Short() {
