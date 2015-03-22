9ded422ee936c675ecf621085f28eae461b7758e
dyv <dvisher1@gmail.com>
2015-03-21 21:14:58 -0400

    changed constants

--- coco/sign/constants.go
+++ coco/sign/constants.go
@@ -7,7 +7,7 @@ import "time"
 var ROUND_TIME time.Duration = 10 * time.Second
 var HEARTBEAT = ROUND_TIME + ROUND_TIME/2
 
-var RoundsPerView int64 = 10
+var RoundsPerView int64 = 100
 
 var FALSE int64 = 0
 var TRUE int64 = 1
