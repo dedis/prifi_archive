a4b12454edaf5fc87d6d1e433f4cf5dfdc2462ff
dyv <dvisher1@gmail.com>
2015-03-21 21:07:36 -0400

    changed constants

--- coco/sign/constants.go
+++ coco/sign/constants.go
@@ -4,10 +4,10 @@ import "time"
 
 // Constants we expect might be used by other packages
 // TODO: can this be replaced by the application using the signer?
-var ROUND_TIME time.Duration = 2 * time.Second
+var ROUND_TIME time.Duration = 10 * time.Second
 var HEARTBEAT = ROUND_TIME + ROUND_TIME/2
 
-var RoundsPerView int64 = 3
+var RoundsPerView int64 = 10
 
 var FALSE int64 = 0
 var TRUE int64 = 1
