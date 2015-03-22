291d02ff3f2747de9b98b9de0c30b90807d4c311
dyv <dvisher1@gmail.com>
2015-03-21 21:08:32 -0400

    changed constants

--- coco/sign/signingNode.go
+++ coco/sign/signingNode.go
@@ -520,7 +520,7 @@ func (sn *Node) Timeout() time.Duration {
 }
 
 func (sn *Node) DefaultTimeout() time.Duration {
-	return 500 * time.Millisecond
+	return 1000 * time.Millisecond
 }
 
 func max(a int64, b int64) int64 {
