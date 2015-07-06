86730c0a761d248cba8d541917eb7cbc540ca5b7
Sam Anklesaria <amsay@amsay.net>
2015-07-05 19:14:39 -0500

    using home directory as base

--- shuf/netchan/mylab/installer.sh
+++ shuf/netchan/mylab/installer.sh
@@ -5,8 +5,11 @@ scp prifi.tar ankles@users.isi.deterlab.net:/proj/SAFER/tarfiles/
 cd ../crypto
 git archive -o crypto.tar HEAD
 scp crypto.tar ankles@users.isi.deterlab.net:/proj/SAFER/tarfiles/
+cd ../prifi/shuf/netchan/mylab
 ssh ankles@users.isi.deterlab.net <<HERE
 cd /proj/SAFER/tarfiles
-tar -xvf crypto.tar -C ~/go/src/github.com/dedis/crypto
-tar -xvf prifi.tar -C ~/go/src/github.com/dedis/prifi
+mkdir -p ~/go/src/github.com/dedis/crypto
+mkdir -p ~/go/src/github.com/dedis/prifi
+tar -xf crypto.tar -C ~/go/src/github.com/dedis/crypto
+tar -xf prifi.tar -C ~/go/src/github.com/dedis/prifi
 HERE
